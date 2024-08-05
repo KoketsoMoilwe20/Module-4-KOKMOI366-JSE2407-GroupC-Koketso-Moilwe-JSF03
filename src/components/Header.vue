8e8fc9eeaa91d3b4fce2e33b479d9717d420c582
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 09:47:52 +0200

    add toggleNavbar method and isNavbarOpen state

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,28 @@
     <header>
         <nav>
             <div>
-                <button></button>
+                <button>
+                    <img src="/online-shop.png">
+                    <span>SwiftCart</span>
+                </button>
+
             </div>
         </nav>
     </header>
 </template>
+
+<script>
+    export default {
+        name: 'Header',
+        data() {
+            return {
+                isNavbarOpen: false,
+            };
+        },
+        methods: {
+            toggleNavbar() {
+                this.isNavbarOpen = !this.isNavbarOpen;
+            },
+        },
+    };
+</script>

33bcdd4e98d83ca1d4a44b0bf98c8ab2e57f8d38
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 21:03:39 +0200

    Create a search-bar and category-filter divs

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,3 +1,13 @@
 <template>
-    
+    <div class="filter-component">
+        <div class="search-bar">
+            <input />
+        </div>
+        <div class="category-filter">
+            <select>
+                <option>All Categories</option>
+                <option></option>
+            </select>
+        </div>
+    </div>
 </template>

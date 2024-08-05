df142b5ea832934c5bfc30a30bcde68f9684666e
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 22:07:52 +0200

    Create Sort.vue

---
+++ src/components/Sort.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="sort-component">
+        <select>
+            <option value="">Sort by</option>
+            <option value="lowToHigh">Price: Low to High</option>
+            <option value="highToLow">Price: High to Low</option>
+        </select>
+    </div>
+</template>

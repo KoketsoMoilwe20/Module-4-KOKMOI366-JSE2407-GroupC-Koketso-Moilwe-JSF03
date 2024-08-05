b7d005c227a325ed186445f33c633b4bf696b759
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 15:09:22 +0200

    import ProductDetails

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import ProductGrid from './components/ProductGrid.vue'
+import ProductDetails from './components/ProductDetails.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [

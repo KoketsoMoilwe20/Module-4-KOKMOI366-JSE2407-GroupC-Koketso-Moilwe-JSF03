6f5226da8784c0d070fb957b6dce4a043469db36
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 15:18:18 +0200

    Add product details template
    - Implemented template for product details view

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="product-details">
-        
+        <img :src="product.image" class="product-image">
+        <h2>{{ product.title }}</h2>
+        <p class="price">${{ product.price }}</p>
+        <p class="description">{{ product.description }}</p>
+        <p class="category">Category: {{ product.category }}</p>
     </div>
 </template>
+
+<script>
+    
+</script>

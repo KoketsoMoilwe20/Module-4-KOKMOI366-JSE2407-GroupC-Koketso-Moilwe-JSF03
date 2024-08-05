06a59817bc50b837a0417b4ef2be879efb1b6812
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 13:11:43 +0200

    add route configuration for Homepage
    Define route path '/' for Home page and associate the Home route with the ProductGrid component

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import App from './App.vue'
 import ProductGrid from './components/ProductGrid.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
-
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: ProductGrid
+    }
+]
 
 createApp(App).mount('#app')

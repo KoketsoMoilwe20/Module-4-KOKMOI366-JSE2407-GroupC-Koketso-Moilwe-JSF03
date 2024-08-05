60dc9a79818ec60ef027ca296024e0dbd49cfad6
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 13:30:24 +0200

    import Vue composition API and Vue Router hooks
    -Import `ref` and `onMounted` from Vue for composition API usage
    -Import `useRouter` from `vue-router` to access router functionality

--- src/components/ProductGrid.vue
+++ src/components/ProductGrid.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="product-grid">
         <div class="grid">
-            
+
         </div>
     </div>
 </template>
+
+<script setup>
+    import {ref, onMounted} from 'vue'
+    import { useRouter } from 'vue-router';
+</script>

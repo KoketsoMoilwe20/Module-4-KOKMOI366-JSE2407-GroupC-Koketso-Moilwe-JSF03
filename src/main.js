18bbe2c61f3c2a19f973e6a0c22872dfd1944bc1
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-08-05 13:09:03 +0200

    import ProductGrid

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ProductGrid from './components/ProductGrid.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
 
 
 createApp(App).mount('#app')
